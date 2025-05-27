a31efab4c772ed25f1fdc1e646b0faeccdd7af39
Vaibhav-30-12 <vaibhav.dabas.ug23@plaksha.edu.in>
2025-05-27 17:26:15 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-sis3.vercel.app/',
+    origin:'http://localhost:8000',
     credentials:true
 }
 

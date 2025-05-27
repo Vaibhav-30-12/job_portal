9500e64753b842ee617da19aba1ea465912f6f28
Vaibhav-30-12 <vaibhav.dabas.ug23@plaksha.edu.in>
2025-05-27 17:59:48 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-taupe-phi.vercel.app',
+    origin:'http://localhost:8000',
     credentials:true
 }
 
